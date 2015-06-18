bffc1f55a33ea6c1756693d220062ff2016629f7
Mark Deraeve <mark.deraeve@gmail.com>
2015-06-18 22:48:07 +0200

    Started the Navigation
    Still Angular 1

---
+++ DailyCardGames/Home/homeController.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+angular.module('Home')
+
+.controller('homeController',
+    ['$scope', '$rootScope', '$state', '$stateParams',
+    function ($scope, $rootScope, $state, $stateParams) {
+
+       //Show available games
+
+    }]);
--- DailyCardGames/app.js
+++ DailyCardGames/app.js
@@ -1,1 +1,29 @@
 ﻿
+// declare modules
+angular.module('Home', []);
+
+var routerApp = angular.module('routerApp', ['ui.router', 'Home']);
+
+routerApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+     .state('home', {
+         url: '/home',
+         templateUrl: '/Home/homeView.html',
+         controller: 'homeController'
+     })
+
+    .state('wiezer', {
+        url: '/wiezer',
+        templateUrl: '/Games/Wiezer/wiezerView.html'
+    })
+
+    .state('kinger', {
+        url: '/kinger',
+        templateUrl: '/Games/Kinger/kingerView.html'
+    })
+
+});
